007825e6d4e5559e72ea8c68ad4413529a85374c
gitlab-runner <do-not-reply@cypress.com>
2022-06-07 14:09:17 -0500

    Upload mtb-example-btsdk-mesh-snip-sensor-client 3.3.0.23146

--- mesh_sensor_client.c
+++ mesh_sensor_client.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2021, Cypress Semiconductor Corporation (an Infineon company) or
+ * Copyright 2016-2022, Cypress Semiconductor Corporation (an Infineon company) or
  * an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
  *
  * This software, including source code, documentation and related
@@ -305,6 +305,7 @@ void mesh_sensor_client_message_handler(uint16_t event, wiced_bt_mesh_event_t* p
         WICED_BT_TRACE("not processed\n");
         break;
     }
+    wiced_bt_mesh_release_event(p_event);
 }
 
 
